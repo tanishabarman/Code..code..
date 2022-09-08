09232accbb42280d929941c1e2094b75013e1796
tanishabarman <71121526+tanishabarman@users.noreply.github.com>
2022-09-09 01:22:38 +0530

    Time: 2 ms (82.55%), Space: 39.7 MB (93.18%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        List<Integer> list=new ArrayList<>();
+        /*List<Integer> list=new ArrayList<>();
         for(int i=0;i<stones.length;i++)
         {
             list.add(stones[i]);
@@ -13,6 +13,25 @@ class Solution {
             else list.set(list.size()-2,list.get(list.size()-1)-list.get(list.size()-2));
             list.remove(list.size()-1);
         }
-        return list.size()==1?list.get(0):0;
+        return list.size()==1?list.get(0):0;*/
+        Queue<Integer> q=new PriorityQueue<>((a,b)->b-a);
+        for(int i:stones)
+        {
+            q.add(i);
+        }
+        while(q.size()>1)
+        {
+            int a=q.peek();
+            q.poll();
+            int b=q.peek();
+            q.poll();
+            if(a==b) continue;
+            else
+            {
+                q.add(a-b);
+            }
+        }
+     if(!q.isEmpty()) return q.poll();
+        else return 0;
     }
 }
