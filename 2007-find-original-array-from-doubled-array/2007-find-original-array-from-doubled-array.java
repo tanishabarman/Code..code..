8176f669cbf6812fe15f02b1ee5069ba881c47a2
tanishabarman <71121526+tanishabarman@users.noreply.github.com>
2022-09-16 01:39:01 +0530

    Time: 61 ms (87.69%), Space: 57.8 MB (95.85%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,16 +1,41 @@
+import java.util.*;
 class Solution {
     public int[] findOriginalArray(int[] changed) {
       if(changed.length%2==1)  return new int[0];
-        int[] temp=new int[changed.length/2];
+        int[] ans=new int[changed.length/2];
         Arrays.sort(changed);
         Queue<Integer> list=new LinkedList<>();
         int i=0;
         for(int num:changed)
         {
-            if(!list.isEmpty() && list.peek()==num) temp[i++]=list.poll()/2;
+            if(!list.isEmpty() && list.peek()==num) ans[i++]=list.poll()/2;
             else list.add(num*2);
         }
         if(list.size()>0) return new int[0];
-        else return temp;
+        else return ans;
+      /*  if(changed.length%2==1)  return new int[0];
+        
+        List<Integer> list=new ArrayList<>();
+        //int[] ans=new int[changed.length/2];
+        Arrays.sort(changed);
+        int n=changed.length;
+        int k=0;
+        for(int i=0;i<n;i++)
+        {
+            
+            int j=i+1;
+            while(changed[j]<=(changed[i]*2))
+            {
+                if(changed[j]==changed[i]*2) list.add(changed[i]);
+            }
+        }
+        if(list.size()!=(n/2)) return new int[0];
+        else 
+        {
+             int[] ans = new int[list.size()];
+             for(int i=0;i<list.size();i++) ans[i]=list.get(i);
+            return ans;
+        }*/
+        
     }
 }
