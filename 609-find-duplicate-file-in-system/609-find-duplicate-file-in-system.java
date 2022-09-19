d709675c5184d7b68ab7da73fd285f860faf01a2
tanishabarman <71121526+tanishabarman@users.noreply.github.com>
2022-09-20 00:21:21 +0530

    Time: 49 ms (63.27%), Space: 71.2 MB (42.89%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.java
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.java
@@ -7,10 +7,7 @@ class Solution {
             String s1=paths[i].substring(0,paths[i].indexOf(' '));
             int op = paths[i].indexOf('(');
             int clo = paths[i].indexOf(')');
-            
-            
-            while(clo!=-1 && op!=-1 && clo<paths[i].length()){
-                
+            while(clo!=-1 && op!=-1 && clo<paths[i].length()){       
                 String s2=paths[i].substring(op+1,clo);
                 if(map.containsKey(s2))
                 {
@@ -30,13 +27,11 @@ class Solution {
                 clo = paths[i].indexOf(')',clo+1);
             }
         }
-        
         List<List<String>> ans = new ArrayList<>();
         for(Map.Entry<String,List<String>> entry: map.entrySet()){
             if(entry.getValue().size()!=1)    
                 ans.add(entry.getValue());        
         }
-            
         return ans;
     }
 }
