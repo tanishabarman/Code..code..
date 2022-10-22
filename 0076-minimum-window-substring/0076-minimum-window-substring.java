9d4ee321caef75c2b3cd2bc36c24adc5207a3b78
tanishabarman <71121526+tanishabarman@users.noreply.github.com>
2022-10-23 01:13:37 +0530

    Time: 4 ms (96.42%), Space: 43.5 MB (83.79%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,42 +1,49 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int l=0;
-        int r=0;
-        String ans=null;
-        Map<Character, Integer> map=new HashMap<>();
-        for(int i=0;i<t.length();i++)
-        {
-            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
+          if(s=="" || t=="")
+            return "";
+        if(s==null || t==null)
+            return "";
+        
+        int n = s.length();
+        int m = t.length();
+        
+        
+        int freq[] = new int[128];
+        int characters = 0;
+        
+        for(int i=0;i<m;i++){
+            freq[t.charAt(i)]++;
+            characters++;
         }
-        int count=0;
-        for(;r<s.length();r++)
-        {
-            if(map.containsKey(s.charAt(r)))
-            {
-                map.put(s.charAt(r),map.get(s.charAt(r))-1);
-                if(map.get(s.charAt(r))==0) count++;
-
+        
+        int start =0, end = 0;
+        int min_length = Integer.MAX_VALUE;
+        int start_index = 0;
+        
+        while(end < n){
+            
+            if(freq[s.charAt(end)]>0)
+                characters--;
+            freq[s.charAt(end)]--;
+            end++;
+            
+            while(characters==0){
+                
+                if(min_length>end-start){
+                    min_length = end-start;
+                    start_index = start;
+                }
+                freq[s.charAt(start)]++;
+                if(freq[s.charAt(start)]>0){
+                    characters++;
+                }
+                start++;
+                
             }
             
-            while(count==map.size())
-            {
-                if(ans==null || (r-l+1)<ans.length())
-                {
-                    ans=s.substring(l,r+1);
-                }
-               // System.out.println(map.get(s.charAt(l)) + " " +ans);
-                if(map.containsKey(s.charAt(l)))
-                {
-                    map.put(s.charAt(l),map.get(s.charAt(l))+1);
-                            if(map.get(s.charAt(l))==1) count--;
-                   // System.out.println(map.get(s.charAt(l)));
-                }
-                            l++;
-               // System.out.println(count);
-            }
-           // System.out.println(s.substring(l,r+1));
         }
-        return ans==null?"":ans;
         
+        return min_length == Integer.MAX_VALUE? "" : s.substring(start_index,start_index+min_length);
     }
 }
